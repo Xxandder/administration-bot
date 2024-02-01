04e4e4b6991ddc9d456800b5f395890ab1b1581f
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-01 15:00:02 +0200

    + relation for usersModel

--- src/packages/users/users.model.ts
+++ src/packages/users/users.model.ts
@@ -1,5 +1,8 @@
 import { RelationMappings, Model } from 'objection';
 import { AbstractModel, DatabaseTableName } from '~/libs/packages/database/database.js';
+import { UserDetailsTableColumnName, RegistrationStageTableColumnName, UsersTableColumnName } from './libs/enums/enums.js';
+import { UserDetailsModel } from './user-details.model.js';
+import { RegistrationStageModel } from './registration-stage.model';
 
 class UserModel extends AbstractModel{
     public chatId!: string;
@@ -7,6 +10,33 @@ class UserModel extends AbstractModel{
 
     public static override get tableName(): string{
         return DatabaseTableName.USERS;
+    }
+
+    public static get relationMappings(): RelationMappings{
+        return {
+            details: {
+                relation: Model.HasOneRelation,
+                modelClass: UserDetailsModel,
+                join: {
+                    from: `${DatabaseTableName.USERS}
+                            .${UsersTableColumnName.ID}`,
+                    to:    `${DatabaseTableName.USER_DETAILS}
+                        .${UserDetailsTableColumnName.USER_ID}`
+                        
+                }
+            },
+            registrationStage: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: RegistrationStageModel,
+                join: {
+                    from: `${DatabaseTableName.USERS}
+                            .${UsersTableColumnName.REGISTRATION_STAGE_ID}`,
+                    to:    `${DatabaseTableName.REGISTRATION_STAGE}
+                        .${RegistrationStageTableColumnName.ID}`
+                        
+                }
+            }
+        }
     }
 }
 
