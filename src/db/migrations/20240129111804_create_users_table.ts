9c112ad7e4fc4a1dab0e1e14ca55ea97aba2b5c6
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-01 10:15:32 +0200

    + users table migration

--- src/db/migrations/20240129111804_create_users_table.ts
+++ src/db/migrations/20240129111804_create_users_table.ts
@@ -1,10 +1,35 @@
 import type { Knex } from "knex";
 
+const TABLE_NAME = 'users';
 
-export async function up(knex: Knex): Promise<void> {
+const ColumnName = {
+    ID: 'id',
+    CHAT_ID: 'chat_id',
+    CREATED_AT: 'created_at',
+    UPDATED_AT: 'updated_at'
+} as const;
+
+function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable(TABLE_NAME, (table)=>{
+        table.increments(ColumnName.ID).primary();
+        table
+            .string(ColumnName.CHAT_ID)
+            .unique()
+            .notNullable();
+        table
+            .dateTime(ColumnName.CREATED_AT)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+        table
+            .dateTime(ColumnName.UPDATED_AT)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+    })
 }
 
 
-export async function down(knex: Knex): Promise<void> {
+function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists(TABLE_NAME);
 }
 
+export { up, down };
