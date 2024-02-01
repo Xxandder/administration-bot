11f1f0b3d35b68a058cf9a0ccc8ded45f729f541
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-01 20:51:58 +0200

    + user repository with findById method

---
+++ src/packages/users/user.repository.ts
@@ -0,0 +1,41 @@
+import { type Repository } from "~/libs/types/types.js";
+import { type UserModel } from "./user.model";
+import { UserEntity } from "./user.entity";
+import { UserRelation } from "./libs/enums/enums.js";
+import { type UserQueryResponse } from './libs/types/types.js';
+import { DEFAULT_REGISTRATION_STAGE_ID } from './libs/constants/constants.js';
+
+class UserRepository implements Repository{
+    private userModel: typeof UserModel;
+
+    constructor(userModel: typeof UserModel){
+        this.userModel = userModel;
+    }
+
+    public async findById(id: number): Promise<UserEntity | null>{
+        const user = await this.userModel
+            .query()
+            .withGraphJoined([UserRelation.DETAILS, UserRelation.RELATION_STAGE])
+            .findById(id)
+            .castTo<UserQueryResponse | undefined>();
+
+        if(!user){
+            return null;
+        }
+
+        return UserEntity.initialize({
+            id: user.id,
+            createdAt: new Date(user.createdAt),
+            updatedAt:  new Date(user.updatedAt),
+            chatId: user.chatId,
+            isRegistered: user.isRegistered,
+            registrationStageId: user.registrationStage?.id ??
+                DEFAULT_REGISTRATION_STAGE_ID,
+            fullName: user.details?.fullName ?? null,
+            phoneNumber: user.details?.phoneNumber ?? null
+        })
+    }
+
+}
+
+export { UserRepository };
