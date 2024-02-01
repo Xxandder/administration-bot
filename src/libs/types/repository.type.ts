4d80a683fd7626b79515ba1584c80f2b5fb78961
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-01 15:19:05 +0200

    + repository type

---
+++ src/libs/types/repository.type.ts
@@ -0,0 +1,10 @@
+type Repository<T = unknown> = {
+    findById(id: number): Promise<T>;
+    findAll(payload?: unknown): Promise<T[]>;
+    create(payload: unknown): Promise<T>;
+    update(payload: unknown): Promise<T>;
+    delete(payload: unknown): Promise<number>;
+  };
+  
+  export { type Repository };
+  
