77f892b1676b214f6b89bd59923127ad1beeec4e
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-01 14:07:16 +0200

    * moved registration_stage and is_registered fields from user_details table to users table

--- src/db/migrations/20240201100724_create_registration_stage_table.ts
+++ src/db/migrations/20240201100724_create_registration_stage_table.ts
@@ -1,12 +1,16 @@
 import type { Knex } from "knex";
 
-const TABLE_NAME = 'registration_stage';
+const REGISTRATION_STAGE_TABLE_NAME = 'registration_stage';
+const USERS_TABLE_NAME = 'users';
+const DEFAULT_REGISTRATION_STAGE_ID = 1;
 
 const ColumnName = {
     ID: 'id',
     NAME: 'name',
     CREATED_AT: 'created_at',
-    UPDATED_AT: 'updated_at'
+    UPDATED_AT: 'updated_at',
+    IS_REGISTERED: 'is_registered',
+    REGISTRATION_STAGE_ID: 'registration_stage_id',
 } as const;
 
 const RegistrationStage = [
@@ -16,8 +20,13 @@ const RegistrationStage = [
     'registered'
 ] as const;
 
+const RelationRule = {
+    CASCADE: 'CASCADE',
+  } as const;
+
+
 async function up(knex: Knex): Promise<void> {
-    await knex.schema.createTable(TABLE_NAME, (table)=>{
+    await knex.schema.createTable(REGISTRATION_STAGE_TABLE_NAME, (table)=>{
         table.increments(ColumnName.ID).primary();
         table
             .string(ColumnName.NAME)
@@ -33,16 +42,35 @@ async function up(knex: Knex): Promise<void> {
             .defaultTo(knex.fn.now());
     })
     for(const registrationStage of RegistrationStage){
-        await knex(TABLE_NAME).insert([
+        await knex(REGISTRATION_STAGE_TABLE_NAME).insert([
             { [ColumnName.NAME]: registrationStage},
         ]);
     }
+    await knex.schema.alterTable(USERS_TABLE_NAME, (table)=>{
+        table
+        .boolean(ColumnName.IS_REGISTERED)
+        .notNullable()
+        .defaultTo(false);
+    table
+        .integer(ColumnName.REGISTRATION_STAGE_ID)
+        .references(ColumnName.ID)
+        .inTable(REGISTRATION_STAGE_TABLE_NAME)
+        .onUpdate(RelationRule.CASCADE)
+        .onDelete(RelationRule.CASCADE)
+        .notNullable()
+        .defaultTo(DEFAULT_REGISTRATION_STAGE_ID);
+    });
+
    
 }
 
 
-function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTableIfExists(TABLE_NAME);
+async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists(REGISTRATION_STAGE_TABLE_NAME);
+    await knex.schema.alterTable(USERS_TABLE_NAME, (table) => {
+        table.dropColumn(ColumnName.IS_REGISTERED);
+        table.dropColumn(ColumnName.REGISTRATION_STAGE_ID);
+    });
 }
 
 export { up, down };
--- src/db/migrations/20240201101824_create_user_details_table.ts
+++ src/db/migrations/20240201101824_create_user_details_table.ts
@@ -1,11 +1,8 @@
 import type { Knex } from "knex";
 
-const DEFAULT_REGISTRATION_STAGE_ID = 1;
-
 const TableName = {
     USERS: 'users',
     USER_DETAILS: 'user_details',
-    REGISTRATION_STAGE: 'registration_stage'
 } as const;
 
 const ColumnName = {
@@ -13,8 +10,6 @@ const ColumnName = {
     USER_ID: 'user_id',
     PHONE_NUMBER: 'phone_number',
     FULL_NAME: 'full_name',
-    IS_REGISTERED: 'is_registered',
-    REGISTRATION_STAGE_ID: 'registration_stage_id',
     CREATED_AT: 'created_at',
     UPDATED_AT: 'updated_at'
 } as const;
@@ -41,18 +36,6 @@ function up(knex: Knex): Promise<void> {
             .string(ColumnName.FULL_NAME)
             .unique()
             .defaultTo(null);
-        table
-            .boolean(ColumnName.IS_REGISTERED)
-            .notNullable()
-            .defaultTo(false);
-        table
-            .integer(ColumnName.REGISTRATION_STAGE_ID)
-            .references(ColumnName.ID)
-            .inTable(TableName.REGISTRATION_STAGE)
-            .onUpdate(RelationRule.CASCADE)
-            .onDelete(RelationRule.CASCADE)
-            .notNullable()
-            .defaultTo(DEFAULT_REGISTRATION_STAGE_ID);
         table
             .dateTime(ColumnName.CREATED_AT)
             .notNullable()
