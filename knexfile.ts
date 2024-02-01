c4a158bf2443f133d4a7c2b0ecb739677cab225c
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-01 12:03:20 +0200

    + migration and changed knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
-import { knexSnakeCaseMappers } from 'objection';
 
 dotenv.config();
+
 module.exports = {
   development: {
     client: process.env?.['DB_DIALECT'] || 'postgresql',
@@ -10,7 +10,9 @@ module.exports = {
       user: process.env?.["DB_USER"],
       password: process.env?.["DB_PASSWORD"]?.toString(),
       database: process.env?.["DB_NAME"],
-      port: parseInt(process.env?.["DB_PORT"] || '5432', 10)
+      port: parseInt(process.env?.["DB_PORT"] || '5432', 10),
+      charset: 'utf8',
+      debug: true,
     },
     pool: {
         min: parseInt(process.env?.["DB_POOL_MIN"] || '2', 10),
@@ -23,30 +25,5 @@ module.exports = {
     seeds: {
       directory: './src/db/seeds',
     },
-    debug: false,
-    ...knexSnakeCaseMappers({ underscoreBetweenUppercaseLetters: true }),
-  },
-  production: {
-    client: process.env?.["DB_DIALECT"] || 'postgresql',
-    connection: {
-      host: process.env?.["DB_HOST"],
-      user: process.env?.["DB_USER"],
-      password: process.env?.["DB_PASSWORD"],
-      database: process.env?.["DB_NAME"],
-      port: parseInt(process.env?.["DB_PORT"] || '5432', 10)
-    },
-    pool: {
-        min: parseInt(process.env?.["DB_POOL_MIN"] || '2', 10),
-        max: parseInt(process.env?.["DB_POOL_MAX"] || '10', 10),
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: './src/db/migrations',
-    },
-    seeds: {
-      directory: './src/db/seeds',
-    },
-    debug: false,
-    ...knexSnakeCaseMappers({ underscoreBetweenUppercaseLetters: true }),
   },
 };
