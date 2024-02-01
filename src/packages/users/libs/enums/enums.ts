b152c374b07c741e321975337c2dfb2a5712385c
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-01 20:38:51 +0200

    + enum for user relations

--- src/packages/users/libs/enums/enums.ts
+++ src/packages/users/libs/enums/enums.ts
@@ -1,3 +1,4 @@
 export { RegistrationStageTableColumnName } from './registration-stage-table-column-name.enum.js';
 export { UserDetailsTableColumnName } from './user-details-table-column-name.enum.js';
 export { UsersTableColumnName } from './users-table-column-name.js';
+export { UserRelation } from './user-relation.enum.js';
