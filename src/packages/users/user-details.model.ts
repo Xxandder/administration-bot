317690228526042a4e3885d8dd68b81bee1ac5de
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-01 14:55:30 +0200

    + relations for userDetails model

--- src/packages/users/user-details.model.ts
+++ src/packages/users/user-details.model.ts
@@ -1,5 +1,7 @@
 import { RelationMappings, Model } from 'objection';
 import { AbstractModel, DatabaseTableName } from '~/libs/packages/database/database.js';
+import { UserDetailsTableColumnName, UsersTableColumnName } from './libs/enums/enums.js';
+import { UserModel } from './users.model.js';
 
 class UserDetailsModel extends AbstractModel{
     public phoneNumber!: string;
@@ -7,6 +9,22 @@ class UserDetailsModel extends AbstractModel{
 
     public static override get tableName(): string{
         return DatabaseTableName.USER_DETAILS;
+    }
+
+    public static get relationMappings(): RelationMappings{
+        return {
+            user: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: UserModel,
+                join: {
+                    from: `${DatabaseTableName.USER_DETAILS}
+                            .${UserDetailsTableColumnName.USER_ID}`,
+                    to:    `${DatabaseTableName.USERS}
+                        .${UsersTableColumnName.ID}`
+                        
+                }
+            }
+        }
     }
 }
 
