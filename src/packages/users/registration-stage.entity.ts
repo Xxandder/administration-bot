c788f6cb02e89dec094fe3be5cba02a449cfcba4
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-03 18:41:39 +0200

    + entity for registration stage

---
+++ src/packages/users/registration-stage.entity.ts
@@ -0,0 +1,105 @@
+import { type Entity } from "~/libs/types/types.js";
+
+class UserEntity implements Entity{
+    private id: number | null;
+
+    private createdAt: Date | null;
+
+    private updatedAt: Date | null;
+
+    private name: string;
+
+    private orderNumber: number | null;
+
+    public constructor({
+        id, 
+        createdAt,
+        updatedAt,
+        name,
+        orderNumber
+    }: {
+        id: number | null;
+        createdAt: Date | null;
+        updatedAt: Date | null;
+        name: string;
+        orderNumber: number | null;
+    }){
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.name = name;
+        this.orderNumber = orderNumber;
+    }
+
+    public static initialize({
+        id, 
+        createdAt,
+        updatedAt,
+        name,
+        orderNumber
+    }: {
+        id: number | null;
+        createdAt: Date | null;
+        updatedAt: Date | null;
+        chatId: string;
+        isRegistered: boolean | null;
+        registrationStageId: number | null;
+        name: string;
+        orderNumber: number | null;
+    }){
+        return new UserEntity({
+            id, 
+            createdAt,
+            updatedAt,
+            name,
+            orderNumber
+        })
+    }
+
+    public static initializeNew({
+        name,
+        orderNumber
+    }: {
+        id: number | null;
+        createdAt: Date | null;
+        updatedAt: Date | null;
+        name: string;
+        orderNumber: number | null;
+    }){
+        return new UserEntity({
+            id: null, 
+            createdAt: null,
+            updatedAt: null,
+            name,
+            orderNumber
+        })
+    }
+
+    public toNewObject(): {
+        name: string;
+        orderNumber: number
+    } {
+        return {
+            name: this.name,
+            orderNumber: this.orderNumber as number
+        }
+    }
+
+    public toObject(): {
+        id: number;
+        createdAt: Date;
+        updatedAt: Date;
+        name: string;
+        orderNumber: number
+    } {
+        return {
+            id: this.id as number,
+            createdAt: this.createdAt as Date,
+            updatedAt: this.updatedAt as Date,
+            name: this.name,
+            orderNumber: this.orderNumber as number
+        }
+    }
+}
+
+export { UserEntity };
