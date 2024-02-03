5fe535d8c492d2e3342c9f135de00a0a8c4194e4
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-03 18:48:44 +0200

    + type for registration stage query response

---
+++ src/packages/users/libs/types/registration-stage-query-response.type.ts
@@ -0,0 +1,9 @@
+type RegistrationStageQueryResponse = {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    name: string;
+    orderNumber: number;
+}
+
+export { type RegistrationStageQueryResponse };
