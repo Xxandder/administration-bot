74589b885c9c735bb445f3dc5dbb8812ea7ea235
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-03 19:11:17 +0200

    + order number table name for registration stage table column name enum

--- src/packages/users/libs/enums/registration-stage-table-column-name.enum.ts
+++ src/packages/users/libs/enums/registration-stage-table-column-name.enum.ts
@@ -1,6 +1,7 @@
 const RegistrationStageTableColumnName = {
     ID: 'id',
     NAME: 'name',
+    ORDER_NUMBER: 'order_number',
     CREATED_AT: 'created_at',
     UPDATED_AT: 'updated_at',
 } as const;
