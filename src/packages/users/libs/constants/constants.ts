8005a0d69d935a28a4f9dfff7b241aacbb1a0368
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-03 19:07:15 +0200

    + constants for min and max column names in registration stage queries

--- src/packages/users/libs/constants/constants.ts
+++ src/packages/users/libs/constants/constants.ts
@@ -1,3 +1,9 @@
 const DEFAULT_REGISTRATION_STAGE_ID = 1;
+const MAX_ORDER_COLUMN_NAME = 'max_order_number';
+const MIN_ORDER_COLUMN_NAME = 'min_order_number';
 
-export { DEFAULT_REGISTRATION_STAGE_ID };
+export {
+     DEFAULT_REGISTRATION_STAGE_ID,
+     MAX_ORDER_COLUMN_NAME,
+     MIN_ORDER_COLUMN_NAME
+};
