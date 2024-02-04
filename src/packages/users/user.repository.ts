56967ca924d2f8b42d220bc8f4f00b5c49c56e2b
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-04 13:30:47 +0200

    + method to create user

--- src/packages/users/user.repository.ts
+++ src/packages/users/user.repository.ts
@@ -2,7 +2,8 @@ import { type Repository } from "~/libs/types/types.js";
 import { type UserModel } from "./user.model.js";
 import { UserEntity } from "./user.entity.js";
 import { UserRelation } from "./libs/enums/enums.js";
-import { type UserQueryResponse } from './libs/types/types.js';
+import { type UserQueryResponse,
+type UserCreateQueryPayload } from './libs/types/types.js';
 import { DEFAULT_REGISTRATION_STAGE_ORDER_NUMBER } from './libs/constants/constants.js';
 import { registrationStageRepository } from './user.js';
 
@@ -39,14 +40,33 @@ class UserRepository implements Repository{
     public async create(entity: UserEntity): Promise<UserEntity>{
         const { chatId } = entity.toNewObject();
 
-        const registrationStage = registrationStageRepository.findByOrderNumber(1);
+        const registrationStage = await registrationStageRepository.findByOrderNumber(DEFAULT_REGISTRATION_STAGE_ORDER_NUMBER);
+        const registrationStageId = registrationStage?.toObject().id;
 
         const user = await this.userModel
             .query()
             .insertGraph({
                 chatId,
-                registrationStageId: DEFAULT_REGISTRATION_STAGE_ORDER_NUMBER
-            })
+                isRegistered: false,
+                registrationStageId: registrationStageId,
+                details: {
+                    phoneNumber: null,
+                    fullName: null
+                }
+            } as UserCreateQueryPayload)
+            .withGraphJoined([UserRelation.DETAILS, UserRelation.RELATION_STAGE])
+            .castTo<UserQueryResponse>();
+        
+        return UserEntity.initialize({
+            id: user.id,
+            createdAt: new Date(user.createdAt),
+            updatedAt:  new Date(user.updatedAt),
+            chatId: user.chatId,
+            isRegistered: user.isRegistered,
+            registrationStageId: user.registrationStage.id,
+            fullName: user.details.fullName ?? null,
+            phoneNumber: user.details.phoneNumber ?? null
+        })
 
     }
 
