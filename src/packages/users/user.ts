7264fa1ec37498ae4f888649c6d2492a3007b5a2
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-06 12:12:08 +0200

    + created userService and userRepository instances for project

--- src/packages/users/user.ts
+++ src/packages/users/user.ts
@@ -1,6 +1,17 @@
 import { RegistrationStageModel } from "./registration-stage.model.js";
 import { RegistrationStageRepository } from './registration-stage.repository.js';
+import { UserModel } from "./user.model.js";
+import { UserRepository } from "./user.repository.js";
+import { UserService } from "./user.service.js";
 
 const registrationStageRepository = new RegistrationStageRepository(RegistrationStageModel);
 
-export { registrationStageRepository };
+const userRepository = new UserRepository(UserModel);
+
+const userService = new UserService({userRepository})
+
+export { 
+    registrationStageRepository, 
+    userService,  
+    userRepository
+};
