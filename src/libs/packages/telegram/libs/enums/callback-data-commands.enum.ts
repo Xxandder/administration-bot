de771bacb14cadea502f6689c045edacce1ba1bb
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-16 19:32:15 +0200

    + create appeal callback command

--- src/libs/packages/telegram/libs/enums/callback-data-commands.enum.ts
+++ src/libs/packages/telegram/libs/enums/callback-data-commands.enum.ts
@@ -1,6 +1,7 @@
 const CallbackDataCommands = {
     GO_BACK: 'go_back',
-    CONFIRM_PERSONAL_DATA: 'confirm_personal_data'
+    CONFIRM_PERSONAL_DATA: 'confirm_personal_data',
+    CREATE_APPEAL: 'create_appeal'
 } as const;
 
 export { CallbackDataCommands };
