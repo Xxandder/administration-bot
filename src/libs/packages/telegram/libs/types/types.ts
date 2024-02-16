3cb59cf1e8d4314126458c368a781325046b61ea
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-16 19:45:45 +0200

    + type for common stage values

--- src/libs/packages/telegram/libs/types/types.ts
+++ src/libs/packages/telegram/libs/types/types.ts
@@ -2,3 +2,4 @@ export { type InlineKeyboard } from './inline-keyboard.type.js';
 export { type MessageData } from './message-data.type.js';
 export { type CommonKeyboard } from './common-keyboard-type.js';
 export { type RegistrationStageValues} from './registration-stage-values.type.js';
+export { type CommonStageValues } from './common-stage-values.type.js';
