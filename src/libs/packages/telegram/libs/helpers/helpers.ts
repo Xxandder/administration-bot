2d3e178664bb2456ac0a143021336b07bf69d4bd
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-17 18:19:39 +0200

    + importing helper for getting actual common message

--- src/libs/packages/telegram/libs/helpers/helpers.ts
+++ src/libs/packages/telegram/libs/helpers/helpers.ts
@@ -1,1 +1,2 @@
 export { getActualRegistrationMessageObject } from './get-actual-registration-message.helper.js';
+export { getActualCommonMessageObject } from './get-actual-common-message.helper.js';
