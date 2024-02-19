bf58aea92db61ba234d3bb378d28cfe8a16374b0
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-19 18:21:15 +0200

    + ContentType enum

---
+++ src/libs/enums/content-type.enum.ts
@@ -0,0 +1,10 @@
+const ContentType = {
+    JSON: 'application/json',
+    JPEG: 'image/jpeg',
+    PNG: 'image/png',
+    MPEG: 'audio/mpeg',
+    TEXT: 'text/plain',
+  } as const;
+  
+  export { ContentType };
+  
