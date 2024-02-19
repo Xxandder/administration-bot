e07f342b7b316ffcf9f6fd2e79d8765c8710b2c1
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-19 18:49:22 +0200

    + testing s3

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,34 @@ import { registrationStageRepository, userService, userRepository} from './packa
 
 import { telegramBot } from './libs/packages/telegram/telegram.js';
 
+
+// Testing of loading images
+
+// import { s3 } from './libs/packages/s3/s3.js';
+// import fs from 'fs';
+// const imagePath = './src/dog.jpg';
+// const readFileToBuffer = (filePath: string): Promise<Buffer> => {
+//     return new Promise((resolve, reject) => {
+//       fs.readFile(filePath, (err, data) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(data);
+//         }
+//       });
+//     });
+//   };
+// const buffer = await readFileToBuffer(imagePath);
+// await s3.sendFile({
+//     fileKey: 'image.jpg', // Имя файла в S3
+//     buffer: Buffer.from(buffer), // Преобразование буфера изображения в Buffer
+//     contentType: 'image/jpeg', // Тип содержимого файла
+//   });
+// ===========
+
 console.log(telegramBot);
+
+
 
 
 //npm run start:tsx
