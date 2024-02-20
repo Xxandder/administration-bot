f13ff7d18d5e744fe11918aa4542d94070165225
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-20 13:04:29 +0200

    + appeal and appeal category table names for db enum

--- src/libs/packages/database/enums/database-table-name.enum.ts
+++ src/libs/packages/database/enums/database-table-name.enum.ts
@@ -2,7 +2,9 @@ const DatabaseTableName = {
     USERS: 'users',
     USER_DETAILS: 'user_details',
     REGISTRATION_STAGE: 'registration_stage',
-    CREATING_APPEAL_STAGE: 'creating_appeal_stage'
+    CREATING_APPEAL_STAGE: 'creating_appeal_stage',
+    APPEAL_CATEGORY: 'appeal_category',
+    APPEAL: 'appeal'
 } as const;
 
 export { DatabaseTableName };
