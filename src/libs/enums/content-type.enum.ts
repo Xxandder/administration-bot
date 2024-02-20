7d36087f25a5428e7dafcad46b8aaceed9499274
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-20 12:58:07 +0200

    - redundant content types

--- src/libs/enums/content-type.enum.ts
+++ src/libs/enums/content-type.enum.ts
@@ -1,9 +1,6 @@
 const ContentType = {
-    JSON: 'application/json',
     JPEG: 'image/jpeg',
     PNG: 'image/png',
-    MPEG: 'audio/mpeg',
-    TEXT: 'text/plain',
   } as const;
   
   export { ContentType };
