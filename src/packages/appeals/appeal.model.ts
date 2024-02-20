5e1a75fb22f7b81217aad9bc336f9db37d3e437c
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-20 19:26:50 +0200

    + relation between appeal model and file model throuth appeal_photos table

--- src/packages/appeals/appeal.model.ts
+++ src/packages/appeals/appeal.model.ts
@@ -3,11 +3,13 @@ import { AbstractModel, DatabaseTableName } from '~/libs/packages/database/datab
 import { 
     AppealTableColumnName, 
     AppealCategoryTableColumnName,
-    AppealRelation
+    AppealRelation,
+    AppealsPhotosTableColumnName
 } from './libs/enums/enums.js';
 
 import { AppealCategoryModel } from './appeal-category.model.js';
 import { UserModel, UsersTableColumnName } from '~/packages/users/user.js';
+import { FileModel } from '~/packages/files/files.js';
 
 class AppealModel extends AbstractModel{
     public latitude!: number;
@@ -38,6 +40,18 @@ class AppealModel extends AbstractModel{
                 join: {
                     from: `${DatabaseTableName.APPEALS}.${AppealTableColumnName.USER_ID}`,
                     to: `${DatabaseTableName.USERS}.${UsersTableColumnName.ID}`
+                }
+            },
+            [AppealRelation.PHOTOS]: {
+                relation: Model.ManyToManyRelation,
+                modelClass: FileModel,
+                join: {
+                    from: `${DatabaseTableName.APPEALS}.${AppealTableColumnName.ID}`,
+                    through: {
+                        from: `${DatabaseTableName.APPEALS_PHOTOS}.${AppealsPhotosTableColumnName.APPEAL_ID}`,
+                        to: `${DatabaseTableName.APPEALS_PHOTOS}.${AppealsPhotosTableColumnName.FILE_ID}`
+                      },
+                    to: `${DatabaseTableName.FILES}.${UsersTableColumnName.ID}`
                 }
             }
         }
