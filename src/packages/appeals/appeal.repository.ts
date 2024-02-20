15666ba4dd6952e5257490bae5118977db33c191
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-20 20:22:48 +0200

    + all find methods

--- src/packages/appeals/appeal.repository.ts
+++ src/packages/appeals/appeal.repository.ts
@@ -1,5 +1,9 @@
 import { type Repository } from '~/libs/types/types.js';
 import { type AppealModel } from './appeal.model.js';
+import { AppealEntity } from './appeal.entity.js';
+
+import { type AppealQueryResponse } from './libs/types/types.js';
+import { AppealRelation } from './libs/enums/enums.js';
 
 class AppealRepository implements Repository{
     private appealModel: typeof AppealModel;
@@ -7,6 +11,87 @@ class AppealRepository implements Repository{
     public constructor(appealModel : typeof AppealModel){
         this.appealModel = appealModel
     }
+
+    public async findById(id: number): Promise<AppealEntity | null> {
+        const appeal = await this.appealModel
+          .query()
+          .withGraphJoined(`${AppealRelation.CATEGORY}`)
+          .findById(id)
+          .castTo<AppealQueryResponse | undefined>()
+          .execute();
+    
+        if (!appeal) {
+          return null;
+        }
+    
+        return AppealEntity.initialize({
+            id: appeal.id,
+            userId: appeal.userId,
+            categoryId: appeal.category.id,
+            latitude: appeal.latitude,
+            longitude: appeal.longitude,
+            description: appeal.description,
+            isFinished: appeal.isFinished,
+            createdAt: new Date(appeal.createdAt),
+            updatedAt: new Date(appeal.updatedAt),
+        });
+    }
+
+    public async findNotFinishedByUserId(userId: number): Promise<AppealEntity | null> {
+        const appeal = await this.appealModel
+          .query()
+          .withGraphJoined(`${AppealRelation.CATEGORY}`)
+          .findOne({userId})
+          .castTo<AppealQueryResponse | undefined>()
+          .execute();
+    
+        if (!appeal) {
+          return null;
+        }
+    
+        return AppealEntity.initialize({
+            id: appeal.id,
+            userId: appeal.userId,
+            categoryId: appeal.category.id,
+            latitude: appeal.latitude,
+            longitude: appeal.longitude,
+            description: appeal.description,
+            isFinished: appeal.isFinished,
+            createdAt: new Date(appeal.createdAt),
+            updatedAt: new Date(appeal.updatedAt),
+        });
+    }
+
+    public async findAllByUserId(userId: number): Promise<AppealEntity[] | null> {
+        const appeals = await this.appealModel
+          .query()
+          .withGraphJoined(`${AppealRelation.CATEGORY}`)
+          .findOne({userId})
+          .castTo<AppealQueryResponse[] | undefined>()
+          .execute();
+    
+        if (!appeals) {
+          return null;
+        }
+        
+        return appeals.map(appeal => AppealEntity.initialize({
+            id: appeal.id,
+            userId: appeal.userId,
+            categoryId: appeal.category.id,
+            latitude: appeal.latitude,
+            longitude: appeal.longitude,
+            description: appeal.description,
+            isFinished: appeal.isFinished,
+            createdAt: new Date(appeal.createdAt),
+            updatedAt: new Date(appeal.updatedAt),
+        }));
+    }
+
+    public delete(id: number): ReturnType<Repository['delete']> {
+        return this.appealModel.query().deleteById(id).execute();
+    }
+
+
 }
 
 export { AppealRepository };
