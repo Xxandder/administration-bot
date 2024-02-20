7d7799c43b4de28de9f5a6a828261b3e4610da6d
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-20 14:22:28 +0200

    + appeals_photos table column names enum

--- src/packages/appeals/libs/enums/enums.ts
+++ src/packages/appeals/libs/enums/enums.ts
@@ -1,3 +1,4 @@
 export { AppealCategoryTableColumnName } from './appeal-category-table-column-name.enum.js';
 export  {AppealTableColumnName } from './appeal-table-column-name.enum.js';
 export { AppealRelation } from './appeal-relation.enums.js';
+export { AppealsPhotosTableColumnName } from './appeals-photos-table-column-name.enum.js';
