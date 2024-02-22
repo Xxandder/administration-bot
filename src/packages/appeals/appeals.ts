638d014e299fb6e9dda86788f88fb9aa67d00a3f
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-22 10:34:31 +0200

    + exported appesl repository and service instances

--- src/packages/appeals/appeals.ts
+++ src/packages/appeals/appeals.ts
@@ -1,2 +1,10 @@
-export { AppealModel } from './appeal.model.js';
 export { AppealTableColumnName } from './libs/enums/enums.js';
+import { AppealModel } from './appeal.model.js';
+import { AppealRepository } from './appeal.repository.js';
+import { AppealService } from './appeal.service.js';
+
+const appealRepository = new AppealRepository(AppealModel);
+
+const appealService = new AppealService(appealRepository);
+
+export { appealService };
