949b9511432a5b41a71c1f892f473883f56da9c9
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-22 16:30:54 +0200

    + photos and categoryId to appeal query response

--- src/packages/appeals/libs/types/appeal-query-response.type.ts
+++ src/packages/appeals/libs/types/appeal-query-response.type.ts
@@ -1,12 +1,20 @@
+type FileQueryResponse = {
+    id: number;
+    filePath: string;
+    contentType: string;
+}
+
 type AppealQueryResponse = {
     id: number;
     createdAt: string;
     updatedAt: string;
     userId: number;
+    categoryId: number;
     category: {
         id: number;
         name: string;
     }
+    photos: FileQueryResponse[]
     latitude: number;
     longitude: number;
     description: string;
