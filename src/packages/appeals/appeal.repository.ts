b2bd1ef8aa7e7b5bd6c4b8cd46295417217934f5
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-22 10:01:41 +0200

    + update methods for appeal repository

--- src/packages/appeals/appeal.repository.ts
+++ src/packages/appeals/appeal.repository.ts
@@ -4,7 +4,8 @@ import { AppealEntity } from './appeal.entity.js';
 
 import { type AppealQueryResponse, 
     type AppealCreateQueryPayload } from './libs/types/types.js';
-import { AppealRelation } from './libs/enums/enums.js';
+import { AppealRelation, AppealTableColumnName } from './libs/enums/enums.js';
+import { String } from 'aws-sdk/clients/batch.js';
 
 class AppealRepository implements Repository{
     private appealModel: typeof AppealModel;
@@ -110,10 +111,172 @@ class AppealRepository implements Repository{
         }));
     }
 
+    public async updateCategory(appealId: number, categoryId: number):
+    Promise<AppealEntity | null>{
+        const appeal = (await this.findById(appealId));
+        if(!appeal){
+            return null;
+        }
+
+        await this.appealModel
+            .query()
+            .patch({categoryId})
+            .where({id: appealId})
+
+        const updatedAppeal = await this.appealModel
+            .query()
+            .withGraphJoined(`${AppealRelation.CATEGORY}`)
+            .findById(appealId)
+            .castTo<AppealQueryResponse>();
+      
+        return AppealEntity.initialize({
+            id: updatedAppeal.id,
+            userId: updatedAppeal.userId,
+            categoryId: updatedAppeal.category.id,
+            latitude: updatedAppeal.latitude,
+            longitude: updatedAppeal.longitude,
+            description: updatedAppeal.description,
+            isFinished: updatedAppeal.isFinished,
+            createdAt: new Date(updatedAppeal.createdAt),
+            updatedAt: new Date(updatedAppeal.updatedAt),
+        });
+    }
+
+    public async updateLongitude(appealId: number, longitude: number):
+    Promise<AppealEntity | null>{
+        const appeal = (await this.findById(appealId));
+        if(!appeal){
+            return null;
+        }
+
+        await this.appealModel
+            .query()
+            .patch({longitude})
+            .where({id: appealId})
+
+        const updatedAppeal = await this.appealModel
+            .query()
+            .withGraphJoined(`${AppealRelation.CATEGORY}`)
+            .findById(appealId)
+            .castTo<AppealQueryResponse>();
+      
+        return AppealEntity.initialize({
+            id: updatedAppeal.id,
+            userId: updatedAppeal.userId,
+            categoryId: updatedAppeal.category.id,
+            latitude: updatedAppeal.latitude,
+            longitude: updatedAppeal.longitude,
+            description: updatedAppeal.description,
+            isFinished: updatedAppeal.isFinished,
+            createdAt: new Date(updatedAppeal.createdAt),
+            updatedAt: new Date(updatedAppeal.updatedAt),
+        });
+    }
+
+    public async updateLatitude(appealId: number, latitude: number):
+    Promise<AppealEntity | null>{
+        const appeal = (await this.findById(appealId));
+        if(!appeal){
+            return null;
+        }
+
+        await this.appealModel
+            .query()
+            .patch({latitude})
+            .where({id: appealId})
+
+        const updatedAppeal = await this.appealModel
+            .query()
+            .withGraphJoined(`${AppealRelation.CATEGORY}`)
+            .findById(appealId)
+            .castTo<AppealQueryResponse>();
+      
+        return AppealEntity.initialize({
+            id: updatedAppeal.id,
+            userId: updatedAppeal.userId,
+            categoryId: updatedAppeal.category.id,
+            latitude: updatedAppeal.latitude,
+            longitude: updatedAppeal.longitude,
+            description: updatedAppeal.description,
+            isFinished: updatedAppeal.isFinished,
+            createdAt: new Date(updatedAppeal.createdAt),
+            updatedAt: new Date(updatedAppeal.updatedAt),
+        });
+    }
+
+    public async updateDescription(appealId: number, description: string):
+    Promise<AppealEntity | null>{
+        const appeal = (await this.findById(appealId));
+        if(!appeal){
+            return null;
+        }
+
+        await this.appealModel
+            .query()
+            .patch({description})
+            .where({id: appealId})
+
+        const updatedAppeal = await this.appealModel
+            .query()
+            .withGraphJoined(`${AppealRelation.CATEGORY}`)
+            .findById(appealId)
+            .castTo<AppealQueryResponse>();
+      
+        return AppealEntity.initialize({
+            id: updatedAppeal.id,
+            userId: updatedAppeal.userId,
+            categoryId: updatedAppeal.category.id,
+            latitude: updatedAppeal.latitude,
+            longitude: updatedAppeal.longitude,
+            description: updatedAppeal.description,
+            isFinished: updatedAppeal.isFinished,
+            createdAt: new Date(updatedAppeal.createdAt),
+            updatedAt: new Date(updatedAppeal.updatedAt),
+        });
+    }
+
+    public async updateIsFinished(appealId: number, isFinished: boolean):
+    Promise<AppealEntity | null>{
+        const appeal = (await this.findById(appealId));
+        if(!appeal){
+            return null;
+        }
+
+        await this.appealModel
+            .query()
+            .patch({isFinished})
+            .where({id: appealId})
+
+        const updatedAppeal = await this.appealModel
+            .query()
+            .withGraphJoined(`${AppealRelation.CATEGORY}`)
+            .findById(appealId)
+            .castTo<AppealQueryResponse>();
+      
+        return AppealEntity.initialize({
+            id: updatedAppeal.id,
+            userId: updatedAppeal.userId,
+            categoryId: updatedAppeal.category.id,
+            latitude: updatedAppeal.latitude,
+            longitude: updatedAppeal.longitude,
+            description: updatedAppeal.description,
+            isFinished: updatedAppeal.isFinished,
+            createdAt: new Date(updatedAppeal.createdAt),
+            updatedAt: new Date(updatedAppeal.updatedAt),
+        });
+    }
+
     public delete(id: number): ReturnType<Repository['delete']> {
         return this.appealModel.query().deleteById(id).execute();
     }
 
+    public async findAll(): ReturnType<Repository['findAll']> {
+        return await Promise.resolve([]);
+    }
+
+    public update(): ReturnType<Repository['update']> {
+        return Promise.resolve(null);
+    }
 
 }
 
