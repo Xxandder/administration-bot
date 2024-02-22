fb5974ab5ed7a6f5b64ee28256492c0fc36dff09
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-22 10:19:49 +0200

    + create, find and update methods for appeal service

--- src/packages/appeals/appeal.service.ts
+++ src/packages/appeals/appeal.service.ts
@@ -1,15 +1,142 @@
 import { type Service } from "~/libs/types/types.js";
 import { type AppealRepository } from "./appeal.repository.js";
-
+import { AppealEntity } from "./appeal.entity.js";
 
 class AppealService implements Service{
-    private appealRepository: typeof AppealRepository;
+    private appealRepository: AppealRepository;
 
-    constructor(appealRepository: typeof AppealRepository){
+    constructor(appealRepository: AppealRepository){
         this.appealRepository = appealRepository;
     }
 
-    
+    public async create(userId: number): 
+        Promise<ReturnType<AppealEntity['toObject']> | null>{
+        const item = await this.appealRepository.create(AppealEntity.initializeNew({
+            userId
+        }))
+        
+        if(!item){
+            return null;
+        }
+
+        return item.toObject();
+    }
+
+    public async findById(appealId: number):        
+        Promise<ReturnType<AppealEntity['toObject']> | null>{
+
+        const item = await this.appealRepository.findById(appealId);
+
+        if(!item){
+            return null;
+        }
+
+        return item.toObject();
+    }
+
+    public async findAllFinishedByUserId(appealId: number):        
+        Promise<ReturnType<AppealEntity['toObject']>[] | null>{
+
+        const items = await this.appealRepository.findAllFinishedByUserId(appealId);
+
+        if(!items || items.length === 0){
+            return null;
+        }
+
+        return items.map(item=>item.toObject());
+    }
+
+    public async findNotFinishedByUserId(appealId: number):        
+        Promise<ReturnType<AppealEntity['toObject']> | null>{
+
+        const item = await this.appealRepository.findNotFinishedByUserId(appealId);
+
+        if(!item){
+            return null;
+        }
+
+        return item.toObject();
+    }
+
+    public async updateCategoryId(appealId: number, categoryId: number):
+        Promise<ReturnType<AppealEntity['toObject']> | null>{
+        const item = await this.findById(appealId);
+        if(!item){
+            return null;
+        }
+
+        const updatedAppeal = await this.appealRepository.updateCategory(appealId, categoryId);
+
+        if(!updatedAppeal){
+            return null;
+        }
+
+        return updatedAppeal.toObject();
+    }
+
+    public async updateDescription(appealId: number, description: string):
+        Promise<ReturnType<AppealEntity['toObject']> | null>{
+        const item = await this.findById(appealId);
+        if(!item){
+            return null;
+        }
+
+        const updatedAppeal = await this.appealRepository.updateDescription(appealId, description);
+
+        if(!updatedAppeal){
+            return null;
+        }
+
+        return updatedAppeal.toObject();
+    }
+
+    public async updateIsFinished(appealId: number, isFinished: boolean):
+        Promise<ReturnType<AppealEntity['toObject']> | null>{
+        const item = await this.findById(appealId);
+        if(!item){
+            return null;
+        }
+
+        const updatedAppeal = await this.appealRepository.updateIsFinished(appealId, isFinished);
+
+        if(!updatedAppeal){
+            return null;
+        }
+
+        return updatedAppeal.toObject();
+    }
+
+    public async updateLatitude(appealId: number, latitude: number):
+        Promise<ReturnType<AppealEntity['toObject']> | null>{
+        const item = await this.findById(appealId);
+        if(!item){
+            return null;
+        }
+
+        const updatedAppeal = await this.appealRepository.updateLatitude(appealId, latitude);
+
+        if(!updatedAppeal){
+            return null;
+        }
+
+        return updatedAppeal.toObject();
+    }
+
+    public async updateLongitude(appealId: number, longitude: number):
+        Promise<ReturnType<AppealEntity['toObject']> | null>{
+        const item = await this.findById(appealId);
+        if(!item){
+            return null;
+        }
+
+        const updatedAppeal = await this.appealRepository.updateLongitude(appealId, longitude);
+
+        if(!updatedAppeal){
+            return null;
+        }
+
+        return updatedAppeal.toObject();
+    }
 
 }
 
