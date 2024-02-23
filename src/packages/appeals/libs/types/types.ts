4eb2e4ee61da0cbf7565ef3be3002a3f81d18766
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-23 09:51:45 +0200

    + type for appeal location query response

--- src/packages/appeals/libs/types/types.ts
+++ src/packages/appeals/libs/types/types.ts
@@ -2,3 +2,4 @@ export { type AppealQueryResponse } from './appeal-query-response.type.js';
 export { type AppealCreateQueryPayload } from './appeal-create-query-payload.type.js';
 export { type FileModelType } from './file-model.type.js';
 export { type AppealLocation } from './appeal-location.type.js';
+export { type AppealLocationQueryResponse } from './appeal-location-query-response.type.js'; 
