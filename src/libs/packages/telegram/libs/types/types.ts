76d9ca73399ee4331d9b62bc9dff2b8d45024538
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-23 16:41:34 +0200

    + type for creating appeal stage values

--- src/libs/packages/telegram/libs/types/types.ts
+++ src/libs/packages/telegram/libs/types/types.ts
@@ -3,3 +3,4 @@ export { type MessageData } from './message-data.type.js';
 export { type CommonKeyboard } from './common-keyboard-type.js';
 export { type RegistrationStageValues} from './registration-stage-values.type.js';
 export { type CommonStageValues } from './common-stage-values.type.js';
+export { type CreatingAppealStageValues } from './creating-appeal-stage-values.type.js';
