d99c7af9cd8ef7990f1a16dbd7ac9da426ecfbd8
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-23 16:16:45 +0200

    + appeal categories array as constant

--- src/libs/packages/telegram/libs/constants/constants.ts
+++ src/libs/packages/telegram/libs/constants/constants.ts
@@ -4,10 +4,28 @@ const MIN_DESCRIPTION_LENGTH = 10;
 const MAX_DESCRIPTION_LENGTH = 255;
 const MAX_NUMBER_OF_PHOTOS = 3;
 
+const Categories = [
+    'Аварійне видалення дерев/гілля',
+    'Не очищено від снігу',
+    'Не покошена трава',
+    'Не посипані доріжки',
+    'Не прибране сміття',
+    'Не прибране опале листя',
+    'Відкритий люк',
+    'Порив трубопроводу',
+    'Відсутнє водопостачання',
+    'Відсутнє освітлення',
+    'Амброзія',
+    'Обрив проводів електромережі',
+    'Загроза опаду бурульок',
+    'Інше',
+] as const;
+
 export { 
         MIN_FULL_NAME_LENGTH,
         MAX_FULL_NAME_LENGTH,
         MIN_DESCRIPTION_LENGTH,
         MAX_DESCRIPTION_LENGTH,
-        MAX_NUMBER_OF_PHOTOS
+        MAX_NUMBER_OF_PHOTOS,
+        Categories
 };
