f366e0c024f52808db76916cc1fee7b21d8bc035
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-23 13:38:30 +0200

    + enum for creating appeal messages

--- src/libs/packages/telegram/libs/enums/enums.ts
+++ src/libs/packages/telegram/libs/enums/enums.ts
@@ -5,3 +5,4 @@ export { CallbackDataCommands } from './callback-data-commands.enum.js';
 export { InlineCommands } from './inline-commands.enum.js';
 export { RegistrationStage } from './registration-stage.enum.js';
 export { CommonStage } from './common-stage.enum.js';
+export { CreatingAppealStageMessage } from './creating-appeal-stage-message.enum.js';
