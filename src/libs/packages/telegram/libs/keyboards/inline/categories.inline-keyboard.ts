d91c08f5d009bf0733e03fa0e2f48f2fc6cde258
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-24 09:56:44 +0200

    + return back button to categories inline keyboard

--- src/libs/packages/telegram/libs/keyboards/inline/categories.inline-keyboard.ts
+++ src/libs/packages/telegram/libs/keyboards/inline/categories.inline-keyboard.ts
@@ -1,12 +1,15 @@
 import { type InlineKeyboard } from "../../types/inline-keyboard.type.js";
 import { Categories } from '../../constants/constants.js';
+import { CallbackDataCommands } from "../../enums/enums.js";
 
 const CategoriesKeyboard: InlineKeyboard = {
     reply_markup: {
         inline_keyboard:
-            Categories.map((value, index)=>{
+            [...Categories.map((value, index)=>{
                 return [{text: value, callback_data: `${value}/${index+1}`}]
-            })
+            }),
+            [{text: "Повернутися", callback_data: CallbackDataCommands.GO_BACK}]
+        ]
     }
 } as const;
 
