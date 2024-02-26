235a9b4ece048f4f5323d3b84b33bda15a473b4d
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-26 23:46:43 +0200

    2 column categories keyboard

--- src/libs/packages/telegram/libs/keyboards/inline/categories.inline-keyboard.ts
+++ src/libs/packages/telegram/libs/keyboards/inline/categories.inline-keyboard.ts
@@ -3,14 +3,42 @@ import { Categories } from '../../constants/constants.js';
 import { CallbackDataCommands } from "../../enums/enums.js";
 import { InlineKeyboardMarkup } from "node-telegram-bot-api";
 
+
+
 const CategoriesKeyboard: InlineKeyboardMarkup  = {
-    inline_keyboard:
-        [...Categories.map((value, index)=>{
-            return [{text: value, callback_data: `category/${index+1}`}]
-        }),
+    inline_keyboard:[
+        [
+            {text: Categories[0], callback_data: `category/${1}`},
+            {text: Categories[1], callback_data: `category/${2}`},
+        ],
+        [
+            {text: Categories[2], callback_data: `category/${3}`},
+            {text: Categories[3], callback_data: `category/${4}`},
+        ],
+        [
+            {text: Categories[4], callback_data: `category/${5}`},
+            {text: Categories[5], callback_data: `category/${6}`},
+        ],
+        [
+            {text: Categories[6], callback_data: `category/${7}`},
+            {text: Categories[7], callback_data: `category/${8}`},
+        ],
+        [
+            {text: Categories[8], callback_data: `category/${9}`},
+            {text: Categories[9], callback_data: `category/${10}`},
+        ],
+        [
+            {text: Categories[10], callback_data: `category/${11}`},
+            {text: Categories[11], callback_data: `category/${12}`},
+        ],
+        [
+            {text: Categories[12], callback_data: `category/${13}`},
+            {text: Categories[13], callback_data: `category/${14}`},
+        ],
         [{text: "Повернутися", callback_data: CallbackDataCommands.GO_BACK}]
     ]
     
 } as const;
+
 
 export { CategoriesKeyboard };
