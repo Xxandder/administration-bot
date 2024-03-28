44904477dfbabe6d70386be32c71115d319f910d
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-03-28 11:15:42 +0200

    + callback command for info button

--- src/libs/packages/telegram/libs/enums/callback-data-commands.enum.ts
+++ src/libs/packages/telegram/libs/enums/callback-data-commands.enum.ts
@@ -3,7 +3,8 @@ const CallbackDataCommands = {
     CONFIRM_PERSONAL_DATA: 'confirm_personal_data',
     CREATE_APPEAL: 'create_appeal',
     CONFIRM_APPEAL: 'confirm_appeal',
-    CONFIRM_PHOTOS: 'conform_photos'
+    CONFIRM_PHOTOS: 'conform_photos',
+    INFO: 'info'
 } as const;
 
 export { CallbackDataCommands };
