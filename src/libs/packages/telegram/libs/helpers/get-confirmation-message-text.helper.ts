fdc227017db9161ff3b43d07c94be96edfd77aba
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-03-29 10:48:08 +0200

    + made bold heading in registration confirmation message

--- src/libs/packages/telegram/libs/helpers/get-confirmation-message-text.helper.ts
+++ src/libs/packages/telegram/libs/helpers/get-confirmation-message-text.helper.ts
@@ -4,7 +4,7 @@ type Parameters = {
 }
 
 const getConfirmationMessageText = ({fullName, phoneNumber}: Parameters) : string =>{
-    const messageText = `*Ваші дані:* \nІм\'я: ${fullName}\nНомер телефону: ${phoneNumber}`;
+    const messageText = `*Ваші дані:* \n*Ім\'я:* ${fullName}\n*Номер телефону:* ${phoneNumber}`;
     return messageText;
 }
 
