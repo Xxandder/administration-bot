96d9d387c32279aba1b040d598a080560d9d8c1f
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-02 10:02:09 +0300

    + callback handler with base callback handler method

---
+++ src/libs/packages/telegram/callback-handler.ts
@@ -0,0 +1,64 @@
+import { TelegramBotService } from "./telegram-bot.service.js"
+import TelegramBot from "node-telegram-bot-api"
+import { UserEntity } from "~/packages/users/user.entity.js"
+import { userService } from "~/packages/users/user.js"
+
+class CallbackHandler{
+    private telegramBotService: TelegramBotService
+
+    constructor(telegramBotService: TelegramBotService){
+        this.telegramBotService = telegramBotService
+    }
+
+    async handleCallbackQuery(query: TelegramBot.CallbackQuery){
+        try{
+            const chatId = query.message?.chat.id.toString();
+
+            if (!chatId) {
+                console.error('Invalid chat ID.');
+                return;
+            }
+            const user = await userService.findByChatId(chatId);
+
+            if (!user) {
+                console.error('User not found.');
+                return;
+            }
+
+            const callbackData = query.data;
+
+            if (!callbackData) {
+                console.error('Invalid callback data.');
+                return;
+            }
+
+            if (!user.isRegistered) {
+                await this.handleRegistrationCallback(callbackData, user);
+            } else if (user.isCreatingAppeal) {
+                await this.handleCreatingAppealCallback(callbackData, user);
+            } else {
+                await this.handleCommonCallback(callbackData, user);
+            }
+
+        }catch (error) {
+            console.error('Error handling callback query:', error);
+            throw error;
+        }
+    }
+
+    async handleRegistrationCallback(callbackData: string, user: ReturnType<UserEntity['toObject']>){
+
+    }
+
+    async handleCreatingAppealCallback(callbackData: string, user: ReturnType<UserEntity['toObject']>){
+        
+    }
+
+    async handleCommonCallback(callbackData: string, user: ReturnType<UserEntity['toObject']>){
+        
+    }
+
+
+}
+
+export { CallbackHandler };
