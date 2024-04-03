cb7b80e9c2091931649c1fb860f6a745b08dacdf
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-03 19:39:04 +0300

    + validation error messages for address

--- src/libs/packages/telegram/libs/enums/validation-error-text-message.enum.ts
+++ src/libs/packages/telegram/libs/enums/validation-error-text-message.enum.ts
@@ -1,5 +1,5 @@
 import { MAX_FULL_NAME_LENGTH, MIN_FULL_NAME_LENGTH,
-MIN_DESCRIPTION_LENGTH, MAX_DESCRIPTION_LENGTH
+MIN_DESCRIPTION_LENGTH, MAX_DESCRIPTION_LENGTH, MIN_ADDRESS_LENGTH, MAX_ADDRESS_LENGTH
  } from '../constants/constants.js';
 
 const ValidationErrorTextMessage = {
@@ -8,8 +8,9 @@ const ValidationErrorTextMessage = {
     NAME_TOO_LONG: `Довжина ім\'я не має перевищувати ${MAX_FULL_NAME_LENGTH} символи завдовшки`,
     NAME_TOO_SHORT: `Довжина ім\'я має бути більше ${MIN_FULL_NAME_LENGTH} символів завдовшки`,
     DESCRIPTION_TOO_SHORT: `Опис має містити що найменше ${MIN_DESCRIPTION_LENGTH} символів`,
-    DESCRIPTION_TOO_LONG: `Опис має містити не більше ${MAX_DESCRIPTION_LENGTH} символів`
-
+    DESCRIPTION_TOO_LONG: `Опис має містити не більше ${MAX_DESCRIPTION_LENGTH} символів`,
+    ADDRESS_TO_SHORT: `Адреса має містити що найменше ${MIN_ADDRESS_LENGTH} символів`,
+    ADDRESS_TO_LONG: `Адреса має містити не більше ${MAX_ADDRESS_LENGTH} символів`,
 }
 
 export { ValidationErrorTextMessage };
