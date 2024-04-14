a8daade5fad4ef986e1152e6dfc053a93526cccd
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-14 13:27:26 +0300

    - redundant validation schemas from backend folder

--- backend/src/libs/packages/telegram/libs/validation-schemas/validation-schemas.ts
+++ backend/src/libs/packages/telegram/libs/validation-schemas/validation-schemas.ts
@@ -1,3 +1,2 @@
 export { fullNameSchema } from './full-name.validation-schema.js';
-export { descriptionSchema } from './description.validation-schema.js';
-export { address } from './address.validation-schema.js';
+export { descriptionSchema, address } from '@shared/index.js';
