71f273f82be6a9a905f053d429a1919762b54c45
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-14 12:51:22 +0300

    * moved some constants to common libs folder

--- shared/index.ts
+++ shared/index.ts
@@ -3,7 +3,9 @@ export { MIN_DESCRIPTION_LENGTH,
     MAX_NUMBER_OF_PHOTOS,
     MAX_ADDRESS_LENGTH,
     MIN_ADDRESS_LENGTH,
-    MAX_FULL_NAME_LENGTH,
-    MIN_FULL_NAME_LENGTH,
     Categories
 } from './src/packages/appeals/appeals.js';
+
+export { MAX_FULL_NAME_LENGTH,
+    MIN_FULL_NAME_LENGTH
+ } from './src/libs/constants/constatns.js'
--- shared/src/packages/appeals/libs/constants/constants.ts
+++ shared/src/packages/appeals/libs/constants/constants.ts
@@ -2,9 +2,7 @@ const MIN_DESCRIPTION_LENGTH = 10;
 const MAX_DESCRIPTION_LENGTH = 255;
 const MAX_NUMBER_OF_PHOTOS = 3;
 const MAX_ADDRESS_LENGTH = 255;
-const MIN_ADDRESS_LENGTH = 5;
-const MAX_FULL_NAME_LENGTH = 255;
-const MIN_FULL_NAME_LENGTH = 3;
+const MIN_ADDRESS_LENGTH = 5
 
 const Categories = [
     'Аварійне видалення дерев/гілля',
@@ -28,7 +26,5 @@ export { MIN_DESCRIPTION_LENGTH,
     MAX_NUMBER_OF_PHOTOS,
     MAX_ADDRESS_LENGTH,
     MIN_ADDRESS_LENGTH,
-    MAX_FULL_NAME_LENGTH,
-    MIN_FULL_NAME_LENGTH,
     Categories
 }
