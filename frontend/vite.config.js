829e11bf9178e89838dcad0fdafaa6da9d657990
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-15 13:50:57 +0300

    + plugin to allow svg imports as react components

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPathsPlugin from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     reactRefresh(),
-    tsconfigPathsPlugin()
+    tsconfigPathsPlugin(),
+    svgrPlugin(),
   ],
 });
