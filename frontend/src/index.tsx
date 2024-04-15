b08574bf527927e2c2b9db018d1108e1d64e839d
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-15 13:13:49 +0300

    + applied styles for App

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import { App } from './pages/App'
+import { App } from './pages/app/App'
 
 import '~/assets/css/styles.scss';
 
--- frontend/src/pages/app/App.tsx
+++ frontend/src/pages/app/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import styles from './styles.module.scss';
+
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, Vite React App!</h1>
+      <h1 className={styles['text-font']}>Hello, Vite React App!</h1>
       <p>Welcome to your new React app powered by Vite.</p>
     </div>
   );
