c1c8810e50106ed745020a1c2e0909a5540a25b5
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-15 18:18:51 +0300

    + header component

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -1,29 +1,39 @@
 
 import styles from './styles.module.scss';
-import { Logo } from './logo/logo';
+import { Logo, Button } from '~/libs/components/components';
+import { getValidClassNames } from '~/libs/helpers/get-valid-class-names.helper';
 
 const Header: React.FC = () => {
 
   return (
-    <div className="header">
-        <Logo/>
-        <div className='container header__container'>
-
-        </div>
-        <div className="logo header__logo">
-
-        </div>
-        <div className="header__menu">
-
-        </div>
-        <div className="header__buttons">
-            <div className="button">
-
+    <div className={styles['header']}>
+       
+        <div className={getValidClassNames(
+          styles['header__container'],
+          '_container'
+        )}>
+          <Logo/>
+         
+          <nav className={styles["header__menu"]}>
+            <div className={styles["header__menu-item"]}>
+              ГОЛОВНА
             </div>
-            <div className="button">
-                
+            <div className={styles["header__menu-item"]}>
+              ПРО НАС
             </div>
+            <div className={styles["header__menu-item"]}>
+              ЗВЕРНЕННЯ ГРОМАДЯН
+            </div>
+            <div className={styles["header__menu-item"]}>
+              НОВИНИ
+            </div>
+          </nav>
+          <div className={styles["header__buttons"]}>
+            <Button iconName='plus' style='square' iconWidth={18} iconHeight={18}/>
+            <Button label='Увійти' style='primary'/>
+          </div>
         </div>
+        
    
     </div>
    
