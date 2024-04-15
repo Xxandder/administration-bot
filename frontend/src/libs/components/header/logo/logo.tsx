dcd869e2be20b35d5350710f7580d0906f7c84aa
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-15 14:41:11 +0300

    + final styles and structure of logo component

--- frontend/src/libs/components/header/logo/logo.tsx
+++ frontend/src/libs/components/header/logo/logo.tsx
@@ -11,12 +11,15 @@ const Logo: React.FC = () => {
         '_ibg')}>
         <img src={logoImage} alt="" />
       </div>
+      <div className="logo__text">
       <div className={styles["logo__first-row"]}>
         Десята фортеця
       </div>
       <div className={styles["logo__second-row"]}>
         Благодійний фонд
       </div>
+      </div>
+    
     </div>
    
   );
