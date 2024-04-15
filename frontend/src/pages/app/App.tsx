d9c6ae2a45771ac027ab766197aff850e1c74ce5
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-15 13:16:30 +0300

    - redundant content and styles

--- frontend/src/pages/app/App.tsx
+++ frontend/src/pages/app/App.tsx
@@ -5,8 +5,7 @@ import styles from './styles.module.scss';
 const App: React.FC = () => {
   return (
     <div>
-      <h1 className={styles['text-font']}>Hello, Vite React App!</h1>
-      <p>Welcome to your new React app powered by Vite.</p>
+     
     </div>
   );
 };
