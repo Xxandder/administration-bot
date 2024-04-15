c8341d22073fa50f802dc72c98b42ba6b43603e1
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-15 14:32:01 +0300

    + getValidClassNames function

--- frontend/src/libs/components/header/logo/logo.tsx
+++ frontend/src/libs/components/header/logo/logo.tsx
@@ -1,12 +1,14 @@
-
-import logoImage from '~/assets/images/logo.svg'
+import { getValidClassNames } from '~/libs/helpers/helpers';
+import logoImage from '~/assets/images/logo.png'
 import styles from './styles.module.scss';
 
 const Logo: React.FC = () => {
 
   return (
     <div className={styles['logo']}>
-      <div className={styles["logo__image"]}>
+      <div className={getValidClassNames(
+        styles["logo__image"],
+        '_ibg')}>
         <img src={logoImage} alt="" />
       </div>
       <div className={styles["logo__first-row"]}>
