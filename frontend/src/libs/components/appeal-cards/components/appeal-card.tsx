d074a3a297ff5495d96fcbaa313ba7430ef91cb2
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-16 23:37:46 +0300

    + appeal-card structure

--- frontend/src/libs/components/appeal-cards/components/appeal-card.tsx
+++ frontend/src/libs/components/appeal-cards/components/appeal-card.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Categories } from '~/libs/constants/constants';
+import { AppealStatus } from '~/libs/enums/enums';
+import { Status } from '../../status/status';
 
-const AppealCard: React.FC = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+type Properties = {
+  appealNumber: number;
+  category: typeof Categories[number];
+  status: keyof typeof AppealStatus
+  date: string;
+}
+
+const AppealCard: React.FC<Properties> = ({
+  appealNumber, 
+  category, 
+  status, 
+  date}) => {
+    return (
+      <div className="appeal-card">
+        <div className="appeal-card__container">
+          <div className="appeal-card__number">
+            Звернення<span>#{appealNumber}</span>
+          </div>
+          <div className="appeal-card__title">
+            {category}
+          </div>
+          <div className="appeal-card__state">
+            <Status type={status}/>
+            <div className="appeal-card__date">
+              {date}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 };
 
 export  { AppealCard };
