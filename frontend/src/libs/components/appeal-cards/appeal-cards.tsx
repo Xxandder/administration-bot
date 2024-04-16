0419ad0e9cd14839c44141cb8a49c3c8ce9b1aed
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-17 00:26:00 +0300

    + appealsCards component structure

--- frontend/src/libs/components/appeal-cards/appeal-cards.tsx
+++ frontend/src/libs/components/appeal-cards/appeal-cards.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { AppealCard } from './components/appeal-card';
 
-const AppealCards: React.FC = () => {
+import { AppealCardsProperties } from './libs/types/types';
+
+type Properties = {
+  appeals: AppealCardsProperties[]
+}
+
+const AppealCards: React.FC<Properties> = ({appeals}) => {
   return (
-    <div>
-    
+    <div className='appeals-cards'>
+     {
+      appeals.map(appeal=>{
+        return <AppealCard 
+                  appealNumber={appeal.appealNumber}
+                  status={appeal.status}
+                  category={appeal.category}
+                  date={appeal.date}
+                />
+      })
+     }
     </div>
   );
 };
