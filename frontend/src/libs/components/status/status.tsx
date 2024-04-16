0f494e4cad2fbbe61c142c21407c0059f4fbeb1b
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-16 17:56:55 +0300

    + status comonent structure

--- frontend/src/libs/components/status/status.tsx
+++ frontend/src/libs/components/status/status.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { AppealStatus } from '~/libs/enums/enums';
+import { getValidClassNames } from '~/libs/helpers/get-valid-class-names.helper';
 
+import styles from './styles.module.scss';
+import { AppealStatusName } from './libs/enums/appeal-status-name.enum';
 
 type Properties = {
     type: keyof typeof AppealStatus
@@ -8,8 +11,11 @@ type Properties = {
 
 const Status: React.FC<Properties> = ({type}) => {
   return (
-    <div className="appeal-status">
-      <span className="appeal-status__text">{AppealStatus[type]}</span>
+    <div className={getValidClassNames(
+        styles['appeal-status'],
+        styles[`appeal-status_${AppealStatusName[type]}`]
+    )}>
+      <span className={styles["appeal-status__text"]}>{AppealStatus[type]}</span>
     </div>
   );
 };
--- frontend/src/libs/enums/appeal-statuses.enum.ts
+++
@@ -1,8 +0,0 @@
-const AppealStatuses = {
-    WAITING: 'Очікує відповіді',
-    IN_WORK: 'В роботі',
-    FINISHED: 'Завершено',
-    CANCELED: 'Скасовано'
-} as const;
-
-export { AppealStatuses };
