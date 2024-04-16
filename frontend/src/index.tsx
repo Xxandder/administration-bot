72fc2ef1b403b01fc962cd15727f3f417a2d1bb7
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-16 15:44:46 +0300

    + router provider component with appeals route

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,28 @@ import '~/assets/css/styles.scss';
 
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import { App } from './pages/app/App'
+import { App } from './pages/app/App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AppRoute } from './libs/enums/enums';
+import { Appeals } from './pages/appeals/appeals';
 
-
+const routes = createBrowserRouter([
+      {
+        path: AppRoute.ROOT,
+        element: <App />,
+        children: [
+            {
+              path: AppRoute.APPEALS,
+              element: < Appeals/>
+            }
+        ]
+      },
+    ])
 
 const root = createRoot(document.querySelector('#root') as HTMLElement);
 
 root.render(
     <StrictMode>
-          <App />
+          <RouterProvider router={routes}/>
     </StrictMode>
   );
--- frontend/src/pages/app/App.tsx
+++ frontend/src/pages/app/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 import { Header } from '~/libs/components/header/header';
+import { Outlet } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <Outlet />
     </div>
   );
 };
