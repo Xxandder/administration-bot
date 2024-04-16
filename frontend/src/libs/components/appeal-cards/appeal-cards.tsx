31016c251dd1352ce633a314ebe9153a83703bf5
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-16 23:26:12 +0300

    + empty appeals component

---
+++ frontend/src/libs/components/appeal-cards/appeal-cards.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppealCard } from './components/appeal-card';
+
+const AppealCards: React.FC = () => {
+  return (
+    <div>
+    
+    </div>
+  );
+};
+
+export  { AppealCards };
---
+++ frontend/src/libs/components/appeal-cards/components/appeal-card.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppealCard: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export  { AppealCard };
