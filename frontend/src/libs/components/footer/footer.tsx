b6d7ccdf993d07b6d1c1d4df30194135eb439a6f
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-17 16:18:15 +0300

    + styles for footer

--- frontend/src/libs/components/footer/footer.tsx
+++ frontend/src/libs/components/footer/footer.tsx
@@ -2,26 +2,42 @@ import React from 'react';
 import { Logo } from '../components';
 import { Icon } from '../components';
 
+import { getValidClassNames } from '~/libs/helpers/get-valid-class-names.helper';
+
+import styles from './styles.module.scss';
+
 const Footer: React.FC = () => {
   return (
-    <div className='footer'>
-        <div className="footer__container _container">
+    <div className={styles['footer']}>
+        <div className={
+            getValidClassNames(styles["footer__container"],
+            "_container")}>
             <Logo/>
-            <div className="footer__socials">
-                <a href="" className="footer__social">
-                    <Icon name="telegram"/>
+            <div className={styles["footer__socials"]}>
+                <a href="" className={styles["footer__social"]}>
+                    <div className={styles['footer__icon-container']}>
+                        <Icon name="telegram"/>
+                    </div>
                 </a>
-                <a href="" className="footer__social">
-                    <Icon name="viber"/>
+                <a href="" className={styles["footer__social"]}>
+                    <div className={styles['footer__icon-container']}>
+                        <Icon name="viber"/>
+                    </div>
                 </a>
-                <a href="" className="footer__social">
-                    <Icon name="youTube"/>
+                <a href="" className={styles["footer__social"]}>
+                    <div className={styles['footer__icon-container']}>
+                        <Icon name="youTube"/>
+                    </div>
                 </a>
-                <a href="" className="footer__social">
-                    <Icon name='instagram'/>
+                <a href="" className={styles["footer__social"]}>
+                    <div className={styles['footer__icon-container']}>
+                        <Icon name='instagram'/>
+                    </div>
                 </a>
-                <a href="" className="footer__social">
-                    <Icon name="facebook"/>
+                <a href="" className={styles["footer__social"]}>
+                    <div className={styles['footer__icon-container']}>
+                        <Icon name="facebook"/>
+                    </div>
                 </a>
             </div>  
         </div>
