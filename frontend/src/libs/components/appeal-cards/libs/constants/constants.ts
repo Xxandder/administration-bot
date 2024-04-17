0c97d36a5e70695d4ab9a043cb97eaf89814cc9e
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-17 13:03:16 +0300

    + imported type

--- frontend/src/libs/components/appeal-cards/libs/constants/constants.ts
+++ frontend/src/libs/components/appeal-cards/libs/constants/constants.ts
@@ -1,12 +1,4 @@
-import { Categories } from "~/libs/constants/constants";
-import { AppealStatus } from "~/libs/enums/enums";
-
-type AppealCardsProperties = {
-    appealNumber: number;
-    category: typeof Categories[number];
-    status: keyof typeof AppealStatus
-    date: string;
-  }
+import { AppealCardsProperties } from "../types/types";
 
 const mockAppeals: AppealCardsProperties[]  = [
     {
