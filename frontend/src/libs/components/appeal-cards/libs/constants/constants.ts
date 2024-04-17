7429790f6dda3b8584b36ce84936bf2736e4b22b
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-17 15:32:04 +0300

    + duplicated mocked appeals

--- frontend/src/libs/components/appeal-cards/libs/constants/constants.ts
+++ frontend/src/libs/components/appeal-cards/libs/constants/constants.ts
@@ -18,7 +18,44 @@ const mockAppeals: AppealCardsProperties[]  = [
         category:"Інше",
         status:"IN_WORK",
         date:"22.05.2024"
+    },
+    {
+        appealNumber:1000,
+        category:"Відсутнє освітлення",
+        status:"WAITING",
+        date:"22.02.2024"
+    },
+    {
+        appealNumber:1001,
+        category:"Загроза опаду бурульок",
+        status:"FINISHED",
+        date:"22.02.2024"
+    },
+    {
+        appealNumber:1002,
+        category:"Інше",
+        status:"IN_WORK",
+        date:"22.05.2024"
+    },
+    {
+        appealNumber:1000,
+        category:"Відсутнє освітлення",
+        status:"WAITING",
+        date:"22.02.2024"
+    },
+    {
+        appealNumber:1001,
+        category:"Загроза опаду бурульок",
+        status:"FINISHED",
+        date:"22.02.2024"
+    },
+    {
+        appealNumber:1002,
+        category:"Інше",
+        status:"IN_WORK",
+        date:"22.05.2024"
     }
+
 
 ]
 
