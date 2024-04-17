c6ba9858d17ac1be2f50ad34608deaed8987e986
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-04-17 13:55:13 +0300

    + new icons

--- frontend/src/libs/components/icon/libs/maps/icon-name-to-icon.map.ts
+++ frontend/src/libs/components/icon/libs/maps/icon-name-to-icon.map.ts
@@ -1,11 +1,15 @@
 import plusIcon from '~/assets/icons/plus-icon.svg';
+import arrowRightIcon from '~/assets/icons/arrow-right.svg';
+import loupeIcon from '~/assets/icons/loupe-icon.svg';
 
 import { type IconName } from '~/libs/types/types.js';
 
 const iconNameToIcon: Record<
   IconName,
   string> = {
-    'plus': plusIcon
+    'plus': plusIcon,
+    'arrow-right': arrowRightIcon,
+    'loupe': loupeIcon
 };
 
 export { iconNameToIcon };
